4c3d0b45bcb15f0e8dcdfee30c80884b4fe5f05a
Saumya <saumya_t@cs.iitr.ac.in>
2022-02-15 18:56:10 +0530

    feat: added control of block

--- classes.py
+++ classes.py
@@ -1,5 +1,42 @@
 import random
-colors = [(255,0,0), (0,255,0), (0,0,255)]
+
+colors = [(0,0,0), (255,0,0), (0,255,0), (0,0,255)]
+
+class Block:
+
+    figures = [
+        [[1, 5, 9, 13], [4, 5, 6, 7]],
+        [[1, 2, 5, 9], [0, 4, 5, 6], [1, 5, 9, 8], [4, 5, 6, 10]],
+        [[1, 2, 6, 10], [5, 6, 7, 9], [2, 6, 10, 11], [3, 5, 6, 7]],  
+        [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]],
+        [[1, 2, 5, 6]],
+    ]
+
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.rot = 0
+        self.typ = random.randint(0,len(self.figures)-1)
+        self.col = random.randint(1,len(colors)-1)
+
+
+    def image(self):
+        return self.figures[self.typ][self.rot]
+
+    def rotate(self):
+        self.rot = (self.rot+1)%(len(self.figures[self.typ]))
+
+    def move_right(self):
+        self.x+=1
+ 
+    def move_left(self):
+        self.x-=1
+
+    def move_down(self):
+        self.y+=1
+
+
 
 
 class Game:
@@ -7,12 +44,12 @@ class Game:
         self.height = height
         self.width = width
         self.side = side
-        self.color = []
+        self.color = [[0]*self.width for i in range(self.height)]
         self.score = 0
         self.active_block = None
-        for i in range(self.height):
-            self.color.append([])
-            for j in range(self.width):
-                self.color[i].append(colors[random.randint(0,len(colors)-1)])
+
+    def reset(self):
+        print("BOARD RESET")
+        self.__init__(20,15,40)
         
         
--- main.py
+++ main.py
@@ -12,42 +12,70 @@ BLACK = (0,0,0)
 x0 = 100
 y0 = 50
 
-
-
 board = classes.Game(20,15,40)
 
-def reset():
-    print("BOARD RESET")
-    board.__init__(20,15,40)
 
 running = True
+
+pressing_down = False
+
+counter = 0 
 while running:
+    counter+=1
+    if counter>100000:
+        counter = 0
+    
+    if board.active_block == None:
+        print("NEW BLOCK CREATED")
+        board.active_block = classes.Block(6, 0)
+    
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                reset()
+                board.reset()
+            if event.key == pygame.K_UP:
+                board.active_block.rotate()
+            if event.key == pygame.K_RIGHT:
+                board.active_block.move_right()
+            if event.key == pygame.K_LEFT:
+                board.active_block.move_left()
+            if event.key == pygame.K_DOWN:
+                pressing_down = True
+                board.active_block.move_down()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                pressing_down = False
 
 
+    if board.active_block is not None:
+        if counter % 100 == 0: 
+            board.active_block.move_down()
+        if pressing_down and counter % 25 == 0: 
+            board.active_block.move_down()
+    
+    #rendering black screen
     screen.fill(BLACK)
 
-
+    #rendering board
     for i in range(board.height):
         for j in range(board.width):
+            #rendering empty board
             pygame.draw.rect(screen,WHITE,[x0+board.side*j,y0+board.side*i,board.side, board.side],1)
-            if i>10 and board.color[i][j]>0:
+            #rendering colors
+            if board.color[i][j]>0:
                pygame.draw.rect(screen,classes.colors[board.color[i][j]],[x0+board.side*j+1,y0+board.side*i+1,board.side-2, board.side-2])
 
-
-    if board.active_block == None:
-        print("NEW BLOCK CREATED")
-        board.active_block = classes.Block(6, 0)
-
-    for x in board.active_block.image():
-        i = math.floor(x/4)
-        j = x%4
-        pygame.draw.rect(screen,classes.colors[board.active_block.col],[x0+board.side*(board.active_block.x+j),y0+board.side*(board.active_block.y+i),board.side-2, board.side-2])
+            
+    #rendering current active block
+    if board.active_block is not None:
+        for x in board.active_block.image():
+            i = math.floor(x/4)
+            j = x%4
+            pygame.draw.rect(screen,classes.colors[board.active_block.col],[x0+board.side*(board.active_block.x+j),y0+board.side*(board.active_block.y+i),board.side-2, board.side-2])
 
     pygame.display.update()
 
